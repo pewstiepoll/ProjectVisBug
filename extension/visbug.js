3bb5a11cd6f74893571a77ea32df12faa3ac6e87
Vadym <wadedevelop@gmail.com>
2019-08-26 23:36:03 +0300

    State machine for extension lifecycle

--- extension/visbug.js
+++ extension/visbug.js
@@ -1,37 +1,69 @@
-const state = {
-  loaded:   {},
-  injected: {},
+import { createTransition, createStateMachine } from "../lib/state-machine"
+
+// extension states (per tab)
+// 1. unloaded
+// 2. loaded
+// 3. injected
+// 4. ejected
+
+// extension state actions (per tab)
+// 1. load  :   unloaded        -> loaded
+// 2. inject:   loaded|ejected  -> injected
+// 3. eject :   injected        -> ready
+
+const extensionStateMachineConfig = {
+  initial: 'unloaded',
+  transitions: {
+    load:   createTransition('unloaded', 'loaded'),
+    inject: createTransition(['loaded', 'ejected'], 'injected'),
+    eject:  createTransition('injected', 'ejected'),
+    unload: createTransition(['loaded', 'ejected', 'injected'], 'unloaded')
+  },
+  methods: {
+    onLoad: () => {
+      chrome.tabs.insertCSS(tab_id,     { file: 'build/bundle.css' })
+      chrome.tabs.executeScript(tab_id, { file: 'web-components.polyfill.js' })
+      chrome.tabs.executeScript(tab_id, { file: 'build/bundle.js' })
+    },
+    onInject: () => {
+      chrome.tabs.executeScript(tab_id, { file: 'toolbar/inject.js' })
+
+      getColorMode()
+    },
+    onEject: () => {
+      chrome.tabs.executeScript(tab_id, { file: 'toolbar/eject.js' })
+    }
+  }
+};
+
+const tabs = {}
+
+export const toggleIn = ({id:tab_id}) => {
+  // if current tab doesn't have extension state yet - create it
+  if (!tabs[tab_id])
+    tabs[tab_id] = createStateMachine(extensionStateMachineConfig)
+
+  const extension = tabs[tab_id]
+
+  switch (extension.state) {
+    case 'loaded':
+        // initial call - load and inject the extension
+        extension.load()
+        extension.inject()
+        break
+    case 'injected':
+        // eject the extension
+        extension.eject()
+        break
+    case 'loaded':
+    case 'ejected':
+        // extension is already loaded - inject it
+        extension.inject()
+        break
+  }
 }
 
-const toggleIn = ({id:tab_id}) => {
-  // toggle out: it's currently loaded and injected
-  if (state.loaded[tab_id] && state.injected[tab_id]) {
-    chrome.tabs.executeScript(tab_id, { file: 'toolbar/eject.js' })
-    state.injected[tab_id] = false
-    // chrome.storage.sync.remove([storagekey])
-  }
-
-  // toggle in: it's loaded and needs injected
-  else if (state.loaded[tab_id] && !state.injected[tab_id]) {
-    chrome.tabs.executeScript(tab_id, { file: 'toolbar/inject.js' })
-    state.injected[tab_id] = true
-    getColorMode()
-  }
-
-  // fresh start in tab
-  else {
-    chrome.tabs.insertCSS(tab_id,     { file: 'toolbar/bundle.css' })
-    chrome.tabs.executeScript(tab_id, { file: 'web-components.polyfill.js' })
-    chrome.tabs.executeScript(tab_id, { file: 'toolbar/bundle.js' })
-    chrome.tabs.executeScript(tab_id, { file: 'toolbar/inject.js' })
-
-    state.loaded[tab_id]    = true
-    state.injected[tab_id]  = true
-    getColorMode()
-  }
-
-  chrome.tabs.onUpdated.addListener(function(tabId) {
-    if (tabId === tab_id)
-      state.loaded[tabId] = false
-  })
-}
+chrome.tabs.onUpdated.addListener(function(tab_id) {
+  // tab was updated - unload its state
+  if (tabs[tab_id]) tabs[tab_id].unload()
+})
